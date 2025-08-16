b576f2112ccce82bffc116c475d515b858c8e805
anwarul islam shawn <shawnanwar29@gmail.com>
2025-08-16 14:28:16 +0600

    Delete Digital Shield-v2.3 directory

--- Digital Shield-v2.3/js/thank_you.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById('settings-link').addEventListener('click', (e) => {
-        e.preventDefault();
-        // This will open the settings page if the user clicks the button
-        browser.runtime.sendMessage({ action: 'open_settings' });
-    });
-});
